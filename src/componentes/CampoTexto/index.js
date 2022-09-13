f107d82fb84b696c1ef161af2b9adae34e21d86a
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-13 19:11:51 -0300

    Feat: componente dos campos de texto no form

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,22 @@
+import './Campotexto.css'
+const CampoTexto = (props) => {
+
+  let valor = 'Maurilio ';
+  const aoDigitado = (e) => {
+    valor = e.target.value;
+    console.log(valor)
+  }
+  return (
+    <div className="campo-texto">
+      <label>
+        {props.label}
+      </label>
+      <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+
+    </div>
+
+  )
+
+}
+
+export default CampoTexto;
