92400c3ed515e644efed8cd1d23c5c3413d6889e
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-13 19:12:15 -0300

    Feat: Componete Banner da Pag

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => {
+
+  return (
+    <header className='banner'>
+      <img src="/imagens/Banner.png" alt="Banner principal da página" />
+    </header>
+
+  )
+
+}
+
+export default Banner;
