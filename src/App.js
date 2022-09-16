a135a13feef581458ee58eeedb09f90ac77b383f
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-16 12:36:50 -0300

    feat : recebendo array do form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 
-
 function App() {
+  const [colaborador, setColaborador] = useState([])
+
+  const aoColaboradorAdd = (colab) => {
+    console.log(colab);
+    setColaborador([...colaborador, colab])
+
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario cadastro=
+        {function (colab) {
+          aoColaboradorAdd(colab)
+
+        }}
+      />
     </div>
   );
 }
