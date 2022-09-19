767467e636f9ef7c164a0101568dfc6c93687124
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-18 23:58:15 -0300

    feat: faz o map e renderiza os cards

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,27 @@
+
+import Card from '../Card'
 import './time.css'
 
 const Time = (props) => {
+
+
   return (
-    <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+    props.colaboradores.length > 0 && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
+
+      <div className='colaboradores'>
+
+        {props.colaboradores.map(function (colaboradores) {
+          return <Card
+            nome={colaboradores.nome}
+            cargo={colaboradores.cargo}
+            imagem={colaboradores.img}
+          />
+
+        })}
+      </div>
+
+
     </section>
   )
 }
